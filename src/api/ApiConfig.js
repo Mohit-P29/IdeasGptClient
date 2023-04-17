cd5a83804aeb8d293bd934f46e59bd31620d8f35
Mohit-P29 <71666621+Mohit-P29@users.noreply.github.com>
2023-04-16 22:34:16 -0400

    Dev branch changes

--- src/api/ApiConfig.js
+++ src/api/ApiConfig.js
@@ -4,7 +4,7 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "http://localhost:5000/",
 });
 
 export default api;
--- src/components/UsersIdeaTable/UsersIdeaTable.js
+++ src/components/UsersIdeaTable/UsersIdeaTable.js
@@ -1,40 +1,36 @@
-import { TableCell, TableContainer, TableHead, TableRow , Table, TableBody, Paper, Button } from "@mui/material";
-import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import { TableCell, TableContainer, TableHead, TableRow, Table, TableBody, Paper, Button } from '@mui/material';
+import React from 'react';
+import ClaimButton from '../ClaimButton';
 
-const UsersIdeaTable = ({likedIdeas}) => {
-    const [ideasData, setIdeasData] = useState([]);
+const UsersIdeaTable = ({ swipedIdeas = [] }) => {
+  return (
+    <>
+      <h1>Liked Ideas</h1>
+      <TableContainer overflow-y='scrollS' component={Paper} id='userProfTable'>
+        <Table options={{ maxBodyHeight: 100 }}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Description</TableCell>
+              <TableCell>Claim</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {swipedIdeas.map((idea) => (
+              <TableRow key={idea.idea}>
+                <TableCell>{idea.ideaText}</TableCell>
+                {!idea.claimed && (<TableCell>
+                  <ClaimButton ideaId={idea.idea} fetchIdea={false} />
+                </TableCell>
+                )
+               }
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
 
-    useEffect(() => {
-        
-    }, []);
-
-    console.log(ideasData);
-
-    return (
-        <TableContainer overflow-y="scrollS" component={Paper} sx={{maxHeight: '200px'}}>
-            <Table options={{maxBodyHeight:100}}>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Description</TableCell>
-                        <TableCell>Claim</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {ideasData.map((idea) => (
-                        <TableRow key={idea._id}>
-                            <TableCell>{idea.text}</TableCell>
-                            <TableCell>
-                                <Button variant="contained" color="primary">
-                                    Claim
-                                </Button>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    )
-}
+};
 
 export default UsersIdeaTable;
